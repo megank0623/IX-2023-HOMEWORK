124f364f35cbfcc4366028ace0fc7d7b0da76ecc
megank0623 <126628817+megank0623@users.noreply.github.com>
2023-06-02 00:03:52 +0200

    working on the hw

--- week_1/day_4/README.md
+++
@@ -1,2 +0,0 @@
-this is a placeholder - put day 4
- homework here later!
---
+++ week_1/day_4/main.js
@@ -0,0 +1,55 @@
+console.log('connected');
+
+const movies = [
+    {title: 'Harry Potter', explanation: 'This movie is about a dude with a stick...', hint: 'It\'s Magic'},
+    {title: 'Just Go With It', explanation: 'This movie is about people who go on holiday...', hint: 'Adam, Drew and Jennifer'},
+    {title: 'Never Back Down', explanation: 'This movie is about two guys with daddy issues who beat each other up...', hint: 'Kanye West - Stronger'},
+    {title: 'Spongebob Squarepants', explanation: 'This movie is about a rectangle...', hint: 'It\'s a cartoon'},
+    {title: '50 First Dates', explanation: 'This movie is about a chick, she has the worst memory...', hint: '50 times...'},
+    {title: 'Cars', explanation: 'In this movie, car go fast...', hint: 'Kachow'},
+    {title: 'Spiderman', explanation: 'In this movie this guy just does not pay his rent, no matter how many times the landlord asks...', hint: 'Peta-Paka'},
+    {title: 'The Wolf Of Wall Street', explanation: 'In this movie there\'s like illegal stuff, lots of money, and a blonde chick...', hint: 'HAWOOooooooooooo'},
+    {title: 'Inception', explanation: 'In this movie everyone is like sleeping all the time...', hint: 'Dreams...'},
+    {title: 'Peter Pan', explanation: 'In this movie some kids die and an angel escorts them to heaven...', hint: 'Always flying, cuz he neverlands'},
+    {title: 'The Lord Of The Rings', explanation: 'In this movie some small guys go for a walk...', hint: 'You will not vacate past this exact position'}
+   ];
+
+let guess = document.getElementById('movie-guess');
+let clueArea = document.getElementById('clue');
+let hintArea = document.getElementById('clue2');
+let warning = document.getElementById('warning');
+let guessCount = 3;
+
+let curMovie;
+
+function setMovie(){
+    curMovie = movies[Math.floor(Math.random() * 11)];
+    clueArea.innerHTML = curMovie.explanation;
+}
+
+setMovie();
+
+
+function checkGuess(event){
+    event.preventDefault();
+    console.log("hello");
+    let userGuess = guess.value;
+    console.log("User guess: " + userGuess);
+
+    //if the user has the correct guess
+    if (userGuess === curMovie.title){
+        setMovie();
+
+    //if the user guessed incorrectly, deduct from their guesses and tell them how many left
+    } else {
+        guessCount--;
+
+        if (guessCount > 0){
+           warning.innerHTML = `Incorrect, you have ${guessCount} guesses left`; 
+        } else {        //QUESTION/stub: how could I get this to show the message for a certain number of times, then go away and reset?
+            warning.innerHTML = 'Incorrect, no more guesses';
+            guessCount = 3;
+        }
+    }
+    
+}
