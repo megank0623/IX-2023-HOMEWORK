5c320e52c5adcdcdd242d4c00ae6521eabeba657
megank0623 <126628817+megank0623@users.noreply.github.com>
2023-06-04 23:24:11 +0200

    fixed everything!!

--- week_1/day_5/main.js
+++ week_1/day_5/main.js
@@ -1,105 +1,123 @@
 console.log("connected");
 
-class Task{
-    constructor(text){
+class TaskRow{
+    constructor(text, completed){
         this.text = text;
+        this.completed = completed;
     }
 
-    static fromJSON(json) {
-        return new Task(json.text);
+    checkOrNo(){
+        //keep track of the status of the checkbox 
+
+        if (this.completed){
+            this.completed = false;
+        } else {
+            this.completed = true;
+        }
+
+        //console.log(`checked change! now: ${this.completed}`);
     }
+
 }
 
 class UI{
     constructor(){
-        this.taskArray = [];        //keep track of the tasks
-        console.log(`starting task array: ${this.taskArray}`)
-        this.form = document.getElementById('form');
-
+        //main IDs from the HTML file
         this.warningLabel = document.getElementById('warning-label');
-        this.warningLabel.innerHTML = "";
-
         this.taskInput = document.getElementById('task-input');
-
         this.tableBody = document.getElementById('table-body');
 
-        this.form.addEventListener('submit', (e) => this.addTask(e));
+        this.taskArray = [];        //fill with the tasks
 
-        //this.loadFromLocalStorage();
+        this.form = document.getElementById('form');
+        this.form.addEventListener('submit', (e) => this.addTask(e)); 
+
     }
 
+    //this is working
     addTask(e){
-        e.preventDefault();
-        let ti = this.taskInput;
-        let ta = this.taskArray;
+        e.preventDefault();     //don't refresh the page on submit
+        //console.log('adding a new event now');
 
-        console.log(`submit clicked, task: ${ti.value}`);
+        let taskText = this.taskInput.value;
+        this.warningLabel.innerHTML = "";       //start this blank
 
-        //if there was nothing input just return
-        if (ti.value === '' ) {
-            this.warningLabel.innerHTML = "Please enter a task before pressing 'add task'";
+        this.taskInput.value = "";      //clear the text input area
+
+        //check to make sure nothing empty was submitted
+        if (taskText === ""){
+            this.warningLabel.innerHTML = "Please insert a task before pressing submit";
             return;
-        } else {
-            this.warningLabel.innerHTML = "";
         }
 
-        const newTask = new Task(ti.value);
-        ta.push(newTask);
+        let task = new TaskRow(taskText, false);
+        //console.log(`input task: ${taskText}`);
+        this.taskArray.push(task);
 
-        //save to local storage?
-        this.updateTable();
+        this.createRow(task);
     }
 
     deleteTask(task){
+        //console.log(`entering the delete function for task: ${task.text}`);
+        this.tableBody.innerHTML = "";
+        task.text = "";
 
-        let ta = this.taskArray;
-        this.taskArray = ta.filter((x) => {
-            return task.text !== x.task;
-        });
-        
-        console.log(`deleted, new task array: ${this.taskArray}`);
-      
-        //save to local storage?
-        this.updateTable();
-    }
+        for (let i = 0; i < this.taskArray.length; i++){
+            const currTask = this.taskArray[i];
 
-    updateTable() {
-        //this.tableBody.innerHTML = '';
-        this.tableBody.value = "";
-    
-        for (let i = 0; i < this.taskArray.length; i++) {
-          const task = this.taskArray[i];
-    
-          const tr = this.createRow(task);
-          this.tableBody.appendChild(tr);
+            if (currTask.text !== ""){
+                this.createRow(currTask);
+            }
         }
-
-        this.taskInput.value = "";
     }
 
     createRow(task){
-        const tr = document.createElement('tr');
+       //console.log('creating a new row');
 
-        const tdTask = document.createElement('td');
-        const tdComplete = document.createElement('input');
-        const tdDeleteButton = document.createElement('button');
+       const tableRow = document.createElement('tr');
+       const delButton = document.createElement('button');
+       const checkBox = document.createElement('input');
 
-        //setting up the task cell
+       //td is for the cells
+       const tdTask = document.createElement('td');
+       const tdCheckBox = document.createElement('td');
+       const tdButton = document.createElement('td');
+       tdButton.appendChild(delButton);
+
+        //changing the attributes
         tdTask.innerHTML = task.text;
 
-        //setting up the checkbox cell
-        tdComplete.type = 'checkbox';
+        tdCheckBox.appendChild(checkBox);
+        checkBox.type = 'checkbox';
+        checkBox.setAttribute('class', 'form-check-input');
+        checkBox.addEventListener('click', () => task.checkOrNo());
+        this.boxSetup(checkBox, task.completed);
 
-        //setting up the delete button
-        tdDeleteButton.setAttribute('class', 'btn btn-outline-danger');
-        tdDeleteButton.innerHTML = '🗑️';
-        tdDeleteButton.addEventListener('click', () => this.deleteTask(task));
+        delButton.setAttribute("class", "btn btn-outline-light");
+        delButton.innerHTML = '🗑️';
+        delButton.addEventListener('click', () => this.deleteTask(task));
 
-        tr.appendChild(tdTask);
-        tr.appendChild(tdComplete);
-        tr.appendChild(tdDeleteButton);
+        //adding things back to the row
+       tableRow.appendChild(tdTask);
+       tableRow.appendChild(tdCheckBox);
+       tableRow.appendChild(tdButton);
 
-        return tr;
+       //adding the row to the table body
+       this.tableBody.appendChild(tableRow);
+
+
+       //maybe add a taskRow object??
+    }
+
+    boxSetup(box, bool){
+        if (bool){
+            //the box should be checked, so set the check box to checked
+            box.setAttribute('id', 'flexCheckChecked');
+            box.setAttribute('checked', true);
+        } else {
+            //the box shouldn't be checked, so set check box to unckeched
+            box.setAttribute('id', 'flexCheckDefault');
+        }
     }
 
 }
