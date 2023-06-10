40c4dbb753bf507b2b84ef13cb5ecf1bdcf13f83
megank0623 <126628817+megank0623@users.noreply.github.com>
2023-06-10 14:09:01 +0200

    auth working

--- week_2/day_9/fire-library/src/components/auth/RegisterPage.js
+++ week_2/day_9/fire-library/src/components/auth/RegisterPage.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+import { auth } from '../../firebase/firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function RegisterPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function onFormSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const userCred = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCred);
+      navigate('/');
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <div>RegisterPage</div>
-  )
+    <div className="container my-5">
+      <div className="card card-body">
+        <h1>Register</h1>
+        <p>Please enter your email and password to register</p>
+
+        <form onSubmit={onFormSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Email Address</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              className="form-control"
+              placeholder="Email"
+            ></input>
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            ></input>
+          </div>
+
+          <div className="d-flex justify-content-end mt-4">
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
---
+++ week_2/day_9/fire-library/src/components/book/BookPage.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+import { useState, useEffect } from 'react';
+
+// If you export default then you don't need the brackets
+import { Book } from '../../models/book';
+import BookForm from './BookForm';
+import BookTable from './BookTable';
+
+import TaskService from '../../services/task-service';
+
+export default function BookPage() {
+      //STATE VARIABLES: books array and book to edit
+  const [books, setBooks] = useState([]);
+  const [bookToEdit, setBookToEdit] = useState(null);
+
+  //FUNCTIONS
+  useEffect(
+    () => {
+      if (!books.length) {
+        onInitialLoad();
+        //console.log(`after onInitialLoad, books:`);
+        //console.log(books);
+      }
+    },
+
+    // In the case of an empty array, the function only fires the first time the
+    // component initializes
+
+    // If we put a variable reference and that variable changes
+    // this function fires again
+    []);
+
+  async function onInitialLoad() {
+    //  try catch here if there is error
+    // show that error to the user
+    try {
+      const books = await TaskService.fetchTasks();
+      setBooks(books);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  async function onBookCreated(book){
+    // create the book
+    const newBook = await TaskService.createTask(new Book(null, book.title, book.author, book.isbn));
+    console.log(`made new book in onBokCret with id ${newBook.id}`);
+    //no book to edit, so set that to null
+    setBookToEdit(null);
+    //add the book onto the already existing books array
+    
+    console.log('books before');
+    console.log(books);
+
+    //put both but why isnt it working
+    setBooks([...books, newBook]);
+
+    console.log('BOOK ADDED, new arary: ');
+    console.log(books);
+  }
+
+  async function onBookEdit(book){
+    //the book we're editing is the book passed
+    setBookToEdit(book);
+
+    //delete this book from the list
+    await TaskService.deleteTask(book.id);
+    setBooks(books.filter((b) => b.id !== book.id));
+  }
+
+  async function onBookDelete(book){
+    //delete it from the database
+    await TaskService.deleteTask(book.id);
+
+    //filter the books array and exclude any with matching isbn
+    setBooks(books.filter((b) => b.id !== book.id));
+  }
+
+  //RETURN 
+  return (
+    <div className='text-center'>
+      <div className='card m-5 p-4'>
+        <BookForm
+          onBookCreated = {onBookCreated}
+          bookToEdit = {bookToEdit}
+        ></BookForm>
+        <BookTable
+          books = {books}
+          onBookEdit = {onBookEdit}
+          onBookDelete = {onBookDelete}
+        ></BookTable>
+      </div>
+    </div>
+  )
+}
